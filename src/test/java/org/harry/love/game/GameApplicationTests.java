c723b49d7e3622c45f18e0ce9978e4d0b861b34c
zhouyiyang.harry <zhouyiyang.harry@bytedance.com>
2020-04-25 19:28:26 +0800

    first commit

---
+++ src/test/java/org/harry/love/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harry.love.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
